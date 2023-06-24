42b1d2b3d1d7223f7a127d4f680ade9b30f4ed51
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-24 19:06:34 +1000

    solved deployment issues, still not sure how to work with interaction

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -2,14 +2,20 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("echo")
+    .setName("test")
     .setDescription("Repeats whatever phrase you input")
+    .addStringOption((option) =>
+      option
+        .setName("input")
+        .setDescription("The input to echo back")
+        .setRequired(true)
+    )
     .addBooleanOption((option) =>
       option
         .setName("ephemeral")
         .setDescription("Whether or not the echo should be ephemeral")
     ),
   async execute(interaction) {
-    await interaction.reply("interaction");
+    await interaction.reply(input);
   },
 };
