f9c7e7c31d23be6bf32806bd59a633b6e2b80f57
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-02 17:00:54 +1000

    added echo template and tested some functions in test command

--- commands/fun/echo.js
+++ commands/fun/echo.js
@@ -2,22 +2,15 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("test")
+    .setName("echo")
     .setDescription("Repeats whatever phrase you input")
     .addStringOption((option) =>
       option
         .setName("input")
         .setDescription("The input to echo back")
         .setRequired(true)
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("ephemeral")
-        .setDescription("Whether or not the echo should be ephemeral")
     ),
   async execute(interaction) {
-    console.log(interaction.options.getRequired);
-    const userInput = interaction.options.getString("input");
-    await interaction.reply(userInput);
+    await interaction.reply("Echo!!");
   },
 };
