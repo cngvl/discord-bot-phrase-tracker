8ad9590c532827c22d72a688bd798364c6fe380c
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-06 19:52:57 +1000

    added message collector template code but wrong placement

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -3,12 +3,9 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("test")
-    .setDescription("Repeats whatever phrase you input")
+    .setDescription("Message collector test function")
     .addStringOption((option) =>
-      option
-        .setName("input")
-        .setDescription("The input to echo back")
-        .setRequired(true)
+      option.setName("input").setDescription("The input to echo back")
     )
     .addBooleanOption((option) =>
       option
@@ -16,8 +13,22 @@ module.exports = {
         .setDescription("Whether or not the echo should be ephemeral")
     ),
   async execute(interaction) {
-    console.log(interaction.options.getRequired);
+    console.log("interaction.options.getRequired");
     const userInput = interaction.options.getString("input");
-    await interaction.reply(userInput);
   },
 };
+
+// `m` is a message object that will be passed through the filter function
+const collectorFilter = (m) => m.content.includes("discord");
+const collector = interaction.channel.createMessageCollector({
+  filter: collectorFilter,
+  time: 15000,
+});
+
+collector.on("collect", (m) => {
+  console.log(`Collected ${m.content}`);
+});
+
+collector.on("end", (collected) => {
+  console.log(`Collected ${collected.size} items`);
+});
