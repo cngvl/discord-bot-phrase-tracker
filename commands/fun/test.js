70e4b8316c22440bc3c9e0cfb82c112297b44b3d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-07 20:12:05 +1000

    tested moving template code elsewhere but didn't work - need to revisit still

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -10,20 +10,19 @@ module.exports = {
   async execute(interaction) {
     console.log("interaction.options.getRequired");
     const userInput = interaction.options.getString("input");
+    // `m` is a message object that will be passed through the filter function
+    const collectorFilter = (m) => m.content.includes("discord");
+    const collector = interaction.channel.createMessageCollector({
+      filter: collectorFilter,
+      time: 15000,
+    });
+
+    collector.on("collect", (m) => {
+      console.log(`Collected ${m.content}`);
+    });
+
+    collector.on("end", (collected) => {
+      console.log(`Collected ${collected.size} items`);
+    });
   },
 };
-
-// `m` is a message object that will be passed through the filter function
-const collectorFilter = (m) => m.content.includes("discord");
-const collector = interaction.channel.createMessageCollector({
-  filter: collectorFilter,
-  time: 15000,
-});
-
-collector.on("collect", (m) => {
-  console.log(`Collected ${m.content}`);
-});
-
-collector.on("end", (collected) => {
-  console.log(`Collected ${collected.size} items`);
-});
