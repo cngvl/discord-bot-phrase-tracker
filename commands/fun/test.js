bf8511ee518cc376b971a18bc778f0b98c3e105e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-19 15:43:59 +1000

    cleaned up code to prep for next method to work on

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("test")
-    .setDescription("Testing feature to figure out messageCollector works"),
+    .setDescription("Testing feature to figure out how certain methods work"),
   async execute(interaction) {
     await interaction.reply({
       embeds: [
@@ -17,25 +17,12 @@ module.exports = {
       time: 1000 * 5,
     });
 
-    // Sends a message in the chat
-    interaction.channel.send("poo");
-
     collector.on("collect", (m) => {
-      console.log("collector.on - collect");
       console.log(m);
-      // if (counter < questions.length) {
-      //   m.channel.send(questions[counter++]);
-      // }
     });
 
     collector.on("end", (collected) => {
-      console.log("collector.on - end");
       console.log(`Collected ${collected.size} messages`);
-
-      // let counter = 0;
-      // collected.forEach((value) => {
-      //   console.log(questions[counter++], value.content);
-      // });
     });
   },
 };
