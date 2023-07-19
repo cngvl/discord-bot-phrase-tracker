41f74ee7464d68aa3ceecae7a92f2de963cfd802
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-19 14:26:23 +1000

    i'm a complete idiot

---
+++ commands/fun/messageCollector.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("messageCollector template")
+    .setDescription("template to use #messageCollector"),
+  async execute(interaction) {
+    await interaction.reply({
+      embeds: [
+        {
+          title: "Collecting messages",
+        },
+      ],
+    });
+
+    const collector = interaction.channel.createMessageCollector({
+      time: 1000 * 5,
+    });
+
+    // Sends a message in the chat
+    // interaction.channel.send("poo");
+
+    collector.on("collect", (m) => {
+      console.log(m);
+    });
+
+    collector.on("end", (collected) => {
+      console.log(`Collected ${collected.size} messages`);
+    });
+  },
+};
