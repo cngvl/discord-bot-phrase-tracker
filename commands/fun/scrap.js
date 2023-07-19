e703d5fd8451a24d4568326f60d294fdfa33a197
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-19 14:28:53 +1000

    removed all code relating to #messageCollector into its own file

--- commands/fun/scrap.js
+++ commands/fun/scrap.js
@@ -6,33 +6,8 @@ module.exports = {
     .setDescription("Storing scrapped code that I might use at a later time"),
   async execute(interaction) {
     await interaction.reply("poo");
-    await interaction.reply("poo");
-    // console.log("START OF Interaction from inside test.js");
-    // console.log(interaction);
-    // console.log("END OF Interaction from inside test.js");
-    // `m` is a message object that will be passed through the filter function
-    // const collectorFilter = (m) => m.content;
-    // const collectorFilter = (m) => m.content.includes("discord");
-    // // const channel = interaction.channel;
 
-    // const collector = interaction.channel.createMessageCollector({
-    //   time: 1000 * 10,
-    // });
-
-    // console.log("collector");
-    // console.log(collector);
-
-    // // This code block doesn't fire because this is INSIDE? an event handler which is probably not what I want?
-    // collector.on("collect", (m) => {
-    //   console.log(`Collect`);
-    //   console.log(`Collected ${m.content}`);
-    // });
-
-    // collector.on("end", (collected) => {
-    //   console.log("end");
-    //   console.log(collected);
-    //   console.log(`Collected ${collected.size} items`);
-    // });
-    // const userInput = interaction.options.getString("input");
+    // Sends a message in the chat
+    // interaction.channel.send("poo");
   },
 };
