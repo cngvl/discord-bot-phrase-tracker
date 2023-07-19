d846aa5dc03d7d9edd97af4b6734fd6ac348f4b3
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-19 15:44:15 +1000

    added scrap code for sending embedded messages

--- commands/fun/scrap.js
+++ commands/fun/scrap.js
@@ -9,5 +9,14 @@ module.exports = {
 
     // Sends a message in the chat
     // interaction.channel.send("poo");
+
+    // Sends an embedded message in chat
+    // interaction.channel.send({
+    //   embeds: [
+    //     {
+    //       title: "End of collecting messages",
+    //     },
+    //   ],
+    // });
   },
 };
