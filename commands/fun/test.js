1f20985af2bb851270d18b2a454fce75644137ad
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-20 14:19:43 +1000

    testing fetch now but not sure why it's only fetching certain messages

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -3,26 +3,36 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("test")
-    .setDescription("Testing feature to figure out how certain methods work"),
+    .setDescription("Testing feature to figure out how #fetch works"),
   async execute(interaction) {
     await interaction.reply({
       embeds: [
         {
-          title: "Check console",
+          title: "Running test command - check console",
         },
       ],
     });
 
-    const collector = interaction.channel.createMessageCollector({
-      time: 1000 * 5,
-    });
+    const date1 = Date.now() - 10000;
+    // console.log(new Date(date1));
+    // console.log(date1);
 
-    collector.on("collect", (m) => {
-      console.log(m);
-    });
-
-    collector.on("end", (collected) => {
-      console.log(`Collected ${collected.size} messages`);
-    });
+    interaction.channel.messages
+      .fetch({
+        limit: 3,
+        cache: false,
+        after: date1, // The date time you want it from
+      })
+      .then((messages) => {
+        console.log(`Received ${messages.size} messages`);
+        console.log(messages);
+        let counter = 1;
+        messages.forEach((message) => {
+          console.log(`Message #${counter}`);
+          console.log(`${message.author.username}\n`);
+          counter += 1;
+        });
+      })
+      .catch(console.error);
   },
 };
