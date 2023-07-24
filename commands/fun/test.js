98a03142757ef1b782e60a122443a2441a8784c7
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-24 13:49:25 +1000

    tested different date parameters but still not working as intended

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -13,24 +13,33 @@ module.exports = {
       ],
     });
 
-    const date1 = Date.now() - 10000;
-    // console.log(new Date(date1));
-    // console.log(date1);
+    dayMS = 86400000;
+    const date = new Date();
+    // const collectorFilter = (m) => m.content.includes("string");
 
     interaction.channel.messages
       .fetch({
         limit: 3,
-        cache: false,
-        after: date1, // The date time you want it from
+        // cache: false,
+        on: date, // The date time you want it from. can also use on:
+        // filter: messages.content.length > 0,
       })
       .then((messages) => {
-        console.log(`Received ${messages.size} messages`);
-        console.log(messages);
+        // console.log(messages);
         let counter = 1;
+        console.log(`Received ${messages.size} messages`);
+        console.log(`Date: ${new Date(date)}`);
+        console.log(`\n`);
         messages.forEach((message) => {
           console.log(`Message #${counter}`);
-          console.log(`${message.author.username}\n`);
+          console.log(`Username: ${message.author.username}`);
+          var utcSeconds = message.createdTimestamp;
+          var tryDate = new Date(utcSeconds);
+          console.log(`Date: ${tryDate}`);
+          // console.log(`ID: ${message.id}`);
+          console.log(`Content: ${message.content}`);
           counter += 1;
+          console.log(`\n`);
         });
       })
       .catch(console.error);
