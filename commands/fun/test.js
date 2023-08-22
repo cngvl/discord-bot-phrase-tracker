251a1fd6944d0af157cee6281c13f8a72ee7ee30
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-08-22 17:43:16 +1000

    solved match error and code looks a bit cleaner, need to run more tests to check for bugs

--- commands/fun/test.js
+++ commands/fun/test.js
@@ -1,6 +1,4 @@
-// TODO: Need to figure out what to do as the surprise function
 // I could make a list of hard coded responses but maybe I can expand by implementing some database?
-// TODO: Account for case sensitivity
 // For the event, I can just ping the target user OR if there is no target user I can just fetch the last few messages and ping whoever with some generic message.
 
 const { SlashCommandBuilder } = require("discord.js");
@@ -14,33 +12,39 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("test")
     .setDescription("Scrap tester file to test for surprise feature")
-    // Selecting for phrase
     .addStringOption((option) =>
       option
         .setName("phrase")
         .setRequired(true)
         .setDescription("What phrase do you want to look for")
     )
-    // Selecting for user
     .addStringOption((option) =>
       option
         .setName("user")
         .setDescription("Use @ to mention the user you'd like to select for")
     ),
   async execute(interaction) {
+    const mentionedTargetRaw = interaction.options.getString("user");
+    const mentionedPhrase = interaction.options.getString("phrase");
+    var mentionedUserParsed = null;
+    var userMentioned = false;
+    var numberPhraseMentions = 0;
+
     function stopCondition(message) {
       return (
-        message.content.toLowerCase().includes("stop!") &&
-        message.author.id == interaction.user.id
+        message.content == "stop!" && message.author.id == interaction.user.id
       );
     }
-    // Sorting out target user type shit
-    const mentionedTargetRaw = interaction.options.getString("user");
-    // Might be able to remove
-    var userMentioned = false;
-    // Should make a 'That user doesn't exist' kinda thing to break when a username isn't declared properly
-    // Potential for this to break when the user would manually input "<@ x>" - This sounds a bit silly but need to consider for safety?
+
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    }
+    const randomIntValue = getRandomIntInclusive(3, 4);
+
     function getUserFromMention(mention) {
+      // Potential for this to break when the user would manually input "<@ x>" - This sounds a bit silly but need to consider for safety?
       if (mention.startsWith("<@") && mention.endsWith(">")) {
         mention = mention.slice(2, -1);
         if (mention.startsWith("!")) {
@@ -49,66 +53,26 @@ module.exports = {
         userMentioned = true;
         return mention;
       } else {
-        console.log(chalk.red.bold("User has mention has FAILED"));
+        interaction.channel.send("That user does not exist");
         return false;
       }
     }
 
-    var mentionedUserParsed = null;
-
-    if (mentionedTargetRaw == null) {
-      console.log(chalk.red.bold("NO user has been targeted - NULL"));
-    } else {
-      mentionedUserParsed = getUserFromMention(mentionedTargetRaw);
-    }
-
-    function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-    }
-
-    const randomIntValue = getRandomIntInclusive(3, 4);
-
-    console.log(`Random number: ${randomIntValue}`);
-    console.log(`Phrase: ${interaction.options.getString("phrase")}`);
-
-    var numberPhraseMentions = 0;
-    const mentionedPhrase = interaction.options.getString("phrase");
-
-    console.log(chalk.bgRedBright("End of execute"));
-
-    await interaction.reply({
-      embeds: [
-        {
-          title: `${interaction.user.username} has set up a surprise!`,
-        },
-      ],
-    });
-
-    // Sorting out phrase type shit
     function checkTargetUserMentionPhrase(message, user, phrase) {
-      // console.log(`message.author.id: ${message.author.id}`);
-      // console.log(`user: ${user}`);
-      // console.log(`message.author.id == user: ${message.author.id == user}`);
-      // console.log(`message.content.include(phrase): ${message.content.includes(phrase)}`);
-      if (
-        message.author.id == user &&
-        message.content.toLowerCase().includes(phrase)
-      ) {
-        console.log(`Passes targetUserMentionPhrase`);
-        return true;
-      }
+      return message.author.id == user && isExactMatch(message.content, phrase);
     }
 
-    const collector = interaction.channel.createMessageCollector();
+    function isExactMatch(mainString, targetString) {
+      const words = mainString.split(" ");
+      return words.includes(targetString);
+    }
 
     function surpriseEventForNoTarget(interaction) {
       console.log("Running surpriseEventForNoTarget method");
       var uniqueUsers = [];
       interaction.channel.messages
         .fetch({
-          limit: 5,
+          limit: 20,
         })
         .then((messages) => {
           messages.forEach((message) => {
@@ -121,8 +85,6 @@ module.exports = {
           });
           const randomUserID =
             uniqueUsers[Math.floor(Math.random() * uniqueUsers.length)];
-          // console.log(`Unique user IDs: ${uniqueUsers}`);
-          // console.log(`Random user ID: ${randomUserID}`);
           interaction.channel.send(`<@${randomUserID}> eats ass`);
         })
         .catch(console.error);
@@ -133,15 +95,33 @@ module.exports = {
       interaction.channel.send(`<@${target}> eats ass`);
     }
 
+    if (mentionedTargetRaw == null) {
+      console.log(chalk.red.bold("NO user has been targeted - NULL"));
+    } else {
+      mentionedUserParsed = getUserFromMention(mentionedTargetRaw);
+    }
+
+    // console.log(`Random number: ${randomIntValue}`);
+    // console.log(`Phrase: ${interaction.options.getString("phrase")}`);
+
+    await interaction.reply({
+      embeds: [
+        {
+          title: `${interaction.user.username} has set up a surprise!`,
+        },
+      ],
+    });
+
+    const collector = interaction.channel.createMessageCollector();
+
     collector.on("collect", (m) => {
       if (numberPhraseMentions == randomIntValue && !userMentioned) {
-        // console.log(`Trigger surprise event`);
         surpriseEventForNoTarget(interaction);
       } else if (numberPhraseMentions == randomIntValue && userMentioned) {
         surpriseEventForTarget(interaction, mentionedUserParsed);
       }
 
-      console.log(m.content);
+      // console.log(m.content);
 
       if (stopCondition(m)) {
         collector.stop();
@@ -149,14 +129,12 @@ module.exports = {
         checkTargetUserMentionPhrase(m, mentionedUserParsed, mentionedPhrase)
       ) {
         numberPhraseMentions += 1;
-        console.log(chalk.bgGreenBright(`MENTIONED USER`));
         console.log(`Phrase mention count: ${numberPhraseMentions}`);
       } else if (
         mentionedUserParsed == null &&
         m.content.toLowerCase().includes(mentionedPhrase)
       ) {
         numberPhraseMentions += 1;
-        console.log(chalk.bgRedBright(`MENTION NULL`));
         console.log(`Phrase mention count: ${numberPhraseMentions}`);
       }
     });
@@ -167,14 +145,6 @@ module.exports = {
           `****** Collected ${collected.size} messages ******\n`
         )
       );
-
-      interaction.channel.send({
-        embeds: [
-          {
-            title: "End of test function",
-          },
-        ],
-      });
     });
   },
 };
